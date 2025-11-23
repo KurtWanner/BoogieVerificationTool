10943c56615755c127f3f4ca4c08ebc42e2bc6df
KurtWanner <kurt.wanner5625@gmail.com>
2025-11-23 17:26:50 -0500

    Testing makefile

---
+++ tool/src/main/java/org/VerificationTool/Boogie/BoogieBaseListener.java
@@ -0,0 +1,52 @@
+// Generated from src/main/java/org/VerificationTool/Boogie/Boogie.g4 by ANTLR 4.13.2
+package org.VerificationTool.Boogie;
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+/**
+ * This class provides an empty implementation of {@link BoogieListener},
+ * which can be extended to create a listener which only needs to handle a subset
+ * of the available methods.
+ */
+@SuppressWarnings("CheckReturnValue")
+public class BoogieBaseListener implements BoogieListener {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterR(BoogieParser.RContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitR(BoogieParser.RContext ctx) { }
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitTerminal(TerminalNode node) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitErrorNode(ErrorNode node) { }
+}
---
+++ tool/src/main/java/org/VerificationTool/Boogie/BoogieListener.java
@@ -0,0 +1,20 @@
+// Generated from src/main/java/org/VerificationTool/Boogie/Boogie.g4 by ANTLR 4.13.2
+package org.VerificationTool.Boogie;
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link BoogieParser}.
+ */
+public interface BoogieListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link BoogieParser#r}.
+	 * @param ctx the parse tree
+	 */
+	void enterR(BoogieParser.RContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link BoogieParser#r}.
+	 * @param ctx the parse tree
+	 */
+	void exitR(BoogieParser.RContext ctx);
+}
